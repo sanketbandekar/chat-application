cfab84c1e1ca96dad4642d29dd61615f5e5c1ddb
sanket <bandekarsanket03@gmail.com>
2021-06-09 21:10:53 +0530

    added sign in page

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app'
+import 'firebase/firebase-auth'
+import 'firebase/firebase-database'
 
 const config = {
     apiKey: "AIzaSyCUATlv9NYGeZ3q8eKnixwFqGrMGRp-ZbQ",
@@ -6,9 +8,13 @@ const config = {
     projectId: "chat-web-app-75314",
     storageBucket: "chat-web-app-75314.appspot.com",
     messagingSenderId: "111449163513",
-    appId: "1:111449163513:web:1ba72f0f3d95c475e44428"
+    appId: "1:111449163513:web:1ba72f0f3d95c475e44428",
+    databaseURL: "https://chat-web-app-75314-default-rtdb.firebaseio.com/"
 };
 
 // eslint-disable-next-line no-unused-vars
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
 
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,61 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite'
+import { auth, database } from '../misc/firebase'
+
 
 const SignIn = () => {
+
+    const signInWithProvider = async (provider) =>{
+        try {
+            const {additionalUserInfo, user} = await auth.signInWithPopup(provider)
+
+            if(additionalUserInfo.isNewUser){
+                await database.ref(`profiles/${user.uid}`).set({
+                    name: user.displayName,
+                    createdAt: firebase.database.ServerValue.TIMESTAMP
+                })
+            }
+
+            Alert.success('Signed in', 4000)
+        } catch (err) {
+            Alert.error(err.message, 4000)
+        }
+        
+        
+    }
+
+    const onFacebookSignin = () => {
+        signInWithProvider( new firebase.auth.FacebookAuthProvider())
+    }
+
+    const onGoogleSignin = () => {
+        signInWithProvider( new firebase.auth.GoogleAuthProvider())
+    }
+
     return (
-        <div>
-            SignIn
-        </div>
+        <Container>
+            <Grid className="mt-page">
+                <Row>
+                    <Col xs={24} md={12} mdOffset={6}>
+                    <Panel>
+                        <div className="text-center">
+                            <h2>Welcome to Chat App</h2>
+                            <p>simple chat application for users</p>
+                        </div>
+                        <div className="mt-3">
+                            <Button block color="blue" onClick={onFacebookSignin}>
+                                <Icon icon="facebook"/> Continue with Facebook
+                            </Button>
+                            <Button block color="green" onClick={onGoogleSignin}>
+                                <Icon icon="google"/> Continue with Google
+                            </Button>
+                        </div>
+                    </Panel>
+                    </Col>
+                </Row>
+            </Grid>
+        </Container>
     )
 }
 

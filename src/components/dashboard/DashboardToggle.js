264c2113276538b6ebc4becb3866ac66e6cd0063
sanket <bandekarsanket03@gmail.com>
2021-06-11 20:07:32 +0530

    started building dashboard

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Button, Drawer, Icon } from 'rsuite'
+import React, { useCallback } from 'react'
+import { Alert, Button, Drawer, Icon } from 'rsuite'
 import { useModalState, useMediaQuery } from '../../misc/custom-hook'
+import { auth } from '../../misc/firebase';
 import Dashboard from './Index';
 
 const DashboardToggle = () => {
 
     const { isOpen, open, close } = useModalState();
     const isMobile = useMediaQuery('(max-width: 992px)')
+
+    const onSignOut = useCallback( () => {
+        auth.signOut()
+
+        Alert.info('Signed Out', 4000);
+
+        close();
+    }, [close] )
 
     return (
         <>
@@ -15,7 +24,7 @@ const DashboardToggle = () => {
             </Button>
 
             <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-                <Dashboard/>
+                <Dashboard onSignOut={onSignOut}/>
             </Drawer>
         </>
     )

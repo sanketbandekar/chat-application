1db4316495799a8ab14372c7ec7d8f321daa034d
sanket <bandekarsanket03@gmail.com>
2021-06-18 19:48:10 +0530

    created current room context with useContextSelector

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {memo} from 'react'
+import { useCurrentRoom } from '../../../context/current-room.context'
 
 const ChatTop = () => {
+
+    const name = useCurrentRoom( v => v.name )
+
     return (
         <div>
-            Top
+            {name}
         </div>
     )
 }
 
-export default ChatTop
+export default memo(ChatTop)
--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -5,6 +5,7 @@ import ChatTop from '../../components/chat-window/top'
 import Messages from '../../components/chat-window/messages'
 import ChatBottom from '../../components/chat-window/bottom'
 import { useRooms } from '../../context/room.context'
+import { CurrentRoomProvider } from '../../context/current-room.context'
 
 const Chat = () => {
 
@@ -21,8 +22,14 @@ const Chat = () => {
         return <h6 className="text-center mt-page">Chat {chatId} not found</h6>
     }
 
+    const {name, description} = currentRoom
+
+    const currentRoomData = { 
+        name, description
+    }
+
     return (
-        <>
+        <CurrentRoomProvider data={currentRoomData}>
             <div className="chat-top">
                 <ChatTop/>
             </div>
@@ -32,7 +39,7 @@ const Chat = () => {
             <div className="chat-bottom">
                 <ChatBottom/>
             </div>
-        </>
+        </CurrentRoomProvider>
     )
 }
 

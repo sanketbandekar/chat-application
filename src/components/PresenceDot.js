bb62c52abe75757dffc3db1799c0a5134c4b6ae6
sanket <bandekarsanket03@gmail.com>
2021-06-20 00:38:58 +0530

    displayed real time preserverance

---
+++ src/components/PresenceDot.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Whisper, Tooltip, Badge } from 'rsuite'
+import { usePresence } from '../misc/custom-hook'
+
+const getColor = presence => {
+    if(!presence){
+        return 'gray'
+    }
+
+    switch (presence.state){
+        case 'online':
+            return 'green'
+        case 'offline':
+            return 'red'
+        default:
+            return 'gray'
+    }
+}
+
+const getText = presence => {
+
+    if(!presence){
+        return 'Unknown State'
+    }
+
+    return presence.state === 'online' ? 'Online' : `Last seen ${new Date(presence.last_changed).toLocaleDateString()}`
+}
+
+const PresenceDot = ( { uid } ) => {
+
+    const presence = usePresence(uid)
+
+    return (
+        <Whisper placement="top" trigger="hover" speaker={ <Tooltip>
+            {getText(presence)}
+          </Tooltip>}>
+        <Badge className="cursor-pointer" style={{ backgroundColor: getColor(presence) }}/>
+    </Whisper>
+    )
+}
+
+export default PresenceDot

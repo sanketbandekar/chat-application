703cb79063933f19949d82538d33ec48e587d4e1
sanket <bandekarsanket03@gmail.com>
2021-06-19 20:04:45 +0530

    displayed chat messages inside chat room

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { database } from '../../../misc/firebase'
+import { transformToArrayWithId } from '../../../misc/helper'
+import MessageItem from './MessageItem'
 
 const Messages = () => {
+
+    const { chatId } = useParams()
+    const [messages, setMessages] = useState(null)
+
+    const isChatEmpty = messages && messages.length === 0
+    const canShowMessg = messages && messages.length > 0
+
+    useEffect( () => {
+        const messageRef = database.ref('/messages')
+
+        messageRef.orderByChild('roomId').equalTo(chatId).on('value', (snap) => {
+            const data = transformToArrayWithId(snap.val())
+
+            setMessages(data)
+
+        })
+
+        return( () => {
+            messageRef.off('value')
+        } )
+    }, [chatId] )
+
     return (
-        <div>
-            Messages
-        </div>
+        <ul className="msg-list custom-scroll">
+            {isChatEmpty && <li>No messages to display</li>}
+            {canShowMessg && messages.map(msg => <MessageItem key={msg.id} message={msg}/>)}
+        </ul>
     )
 }
 
